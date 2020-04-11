c2ec46145f892bc0ef6281e8dff15648a11255ba
Buddy Scharfenberg <buddy65@gmail.com>
2020-04-11 13:20:22 -0500

    trying to update again.

--- dist/login.php
+++ dist/login.php
@@ -5,18 +5,13 @@
 <html lang="en">
 
 <head>
-
- <meta charset="utf-8">
-
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
- <meta name="description" content="">
-
- <meta name="author" content="">
-
- <title>SB Admin - Start Bootstrap Template</title>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Page Title - CS2300 Store Manager</title>
+  <link href="css/styles.css" rel="stylesheet" />
  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
 </head>
 
