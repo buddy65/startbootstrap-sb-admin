9b23617e1ea1b19e1da9160845cba4504e8445b2
Buddy Scharfenberg <buddy65@gmail.com>
2020-04-12 17:44:39 -0500

    Updating top again.

--- dist/navbartop.php
+++ dist/navbartop.php
@@ -1,6 +1,45 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">Dashboard</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    <a class="navbar-brand" href="index.php">CS2300 Store Manager</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
     <!-- Navbar-->
+
+
+    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+
+    <a class="nav-link" href="index.php">
+
+    <i class="fa fa-fw fa-dashboard"></i>
+
+    <span class="nav-link-text">Dashboard</span>
+
+    </a>
+
+    </li>
+
+    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+
+    <a class="nav-link" href="charts.html">
+
+    <i class="fa fa-check-square"></i>
+
+    <span class="nav-link-text">Create Products</span>
+
+    </a>
+
+    </li>
+
+    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+
+    <a class="nav-link" href="register.php">
+
+    <i class="fa fas fa-user"></i>
+
+    <span class="nav-link-text">Register Users</span>
+
+    </a>
+
+    </li>
+
+
     <ul class="navbar-nav ml-auto ml-md-0">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
